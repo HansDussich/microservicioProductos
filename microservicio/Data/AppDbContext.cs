ca900a5640a49010cc43e30260fbdf228b75a3c7
HansDussich <hansdussich2004@gmail.com>
2025-03-16 21:48:58 -0500

    Agregar archivos de proyecto.

---
+++ microservicio/Data/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using microservicio.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+namespace microservicio.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<Producto> Productos { get; set; }
+    }
+}
